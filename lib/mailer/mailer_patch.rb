6bf93997a328fd18750d3abe6a1ebce9064caf11
Jarrett Chisholm <j.chisholm@chisholmsoft.com>
2014-07-09 14:45:28 -0400

    - upgraded patch to work (mostly) with redmine 2.4.2 (wiki updates email don't seem to work)

--- lib/mailer/mailer_patch.rb
+++ lib/mailer/mailer_patch.rb
@@ -15,9 +15,8 @@ module MailerControlPatch
 end
 
 module InstanceMethods
-  def issue_edit_with_send_mail_control(journal)
-
-    issue = journal.journalized.reload
+  def issue_edit_with_send_mail_control(journal, to_users, cc_users)
+    issue = journal.journalized
     redmine_headers 'Project' => issue.project.identifier,
                     'Issue-Id' => issue.id,
                     'Issue-Author' => issue.author.login
@@ -25,49 +24,53 @@ module InstanceMethods
     message_id journal
     references issue
     @author = journal.user
-    recipients = journal.recipients
-    # Watchers in cc
-    cc = journal.watcher_recipients - recipients
     s = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] "
     s << "(#{issue.status.name}) " if journal.new_value_for('status_id')
     s << issue.subject
     @issue = issue
-    @journal = journal
-    @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue, :anchor => "change-#{journal.id}")
+    
     # plugin modification
     if @issue.mail_checker_issue.nil?
-      recipients = []
-      cc = []
+      to_users = []
+      cc_users = []
     end
     #end
-    mail :to => recipients,
-      :cc => cc,
+    
+    @users = to_users + cc_users
+    @journal = journal
+    @journal_details = journal.visible_details(@users.first)
+    @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue, :anchor => "change-#{journal.id}")
+     mail :to => to_users.map(&:mail),
+      :cc => cc_users.map(&:mail),
       :subject => s
   end
   
-    def issue_add_with_send_mail_control(issue)
+    def issue_add_with_send_mail_control(issue, to_users, cc_users)
     redmine_headers 'Project' => issue.project.identifier,
                     'Issue-Id' => issue.id,
                     'Issue-Author' => issue.author.login
     redmine_headers 'Issue-Assignee' => issue.assigned_to.login if issue.assigned_to
     message_id issue
+    references issue
     @author = issue.author
     @issue = issue
-    @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue)
-    recipients = issue.recipients
-    cc = issue.watcher_recipients - recipients
+    
     # plugin modification
     if @issue.mail_checker_issue.nil?
-      recipients = []
-      cc = []
+      to_users = []
+      cc_users = []
     end
     #end
-    mail :to => recipients,
-      :cc => cc,
+    
+    @users = to_users + cc_users
+    @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue)
+    mail :to => to_users.map(&:mail),
+      :cc => cc_users.map(&:mail),
       :subject => "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] (#{issue.status.name}) #{issue.subject}"
   end
 
   def wiki_content_updated_with_send_mail_control(wiki_content)
+    Rails.logger.error "start"
     redmine_headers 'Project' => wiki_content.project.identifier,
                     'Wiki-Page-Id' => wiki_content.page.id
     @author = wiki_content.author
@@ -87,9 +90,12 @@ module InstanceMethods
       cc = []
     end
     #end
+    
+    Rails.logger.error "end"
     mail :to => recipients,
       :cc => cc,
       :subject => "[#{wiki_content.project.name}] #{l(:mail_subject_wiki_content_updated, :id => wiki_content.page.pretty_title)}"
+    Rails.logger.error "end2"
   end
   
 
